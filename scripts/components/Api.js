fb7397a1b135590ca1630cf24dc165604db26227
flpzht <flpcsb@hotmail.com>
2026-03-24 23:00:43 -0300

    WP: Implementando API

--- scripts/components/Api.js
+++ scripts/components/Api.js
@@ -44,12 +44,10 @@
 //     Authorization: "63d45159-fd92-477c-9dd6-c2859fb9ad61",
 //     "Content-Type": "application/json",
 //   },
-//   body: JSON.stringify(
-//     {
-//       name: "Vale de Yosemite",
-//       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-//     }
-//   ),
+//   body: JSON.stringify({
+//     name: "Vale de Yosemite",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+//   }),
 // })
 //   .then((res) => res.json())
 //   .then((result) => {
@@ -68,23 +66,117 @@
 //   });
 
 // fetch(`https://around-api.pt-br.tripleten-services.com/v1/cards/${card._id}/likes`, {
-//   method: "POST",
-//   headers: {
-//     Authorization: "63d45159-fd92-477c-9dd6-c2859fb9ad61",
+//     method: "POST",
+//     headers: {
+//       Authorization: "63d45159-fd92-477c-9dd6-c2859fb9ad61",
+//     },
 //   },
-// })
+// )
 //   .then((res) => res.json())
 //   .then((result) => {
 //     console.log(result);
-//   })
+//   });
 
-//   fetch(`https://around-api.pt-br.tripleten-services.com/v1/cards/${card._id}/likes`, {
-//   method: "DELETE",
+// fetch(`https://around-api.pt-br.tripleten-services.com/v1/cards/${card._id}/likes`, {
+//     method: "DELETE",
+//     headers: {
+//       Authorization: "63d45159-fd92-477c-9dd6-c2859fb9ad61",
+//     },
+//   },
+// )
+//   .then((res) => res.json())
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+// fetch(`https://around-api.pt-br.tripleten-services.com/v1/users/me/avatar`, {
+//   method: "PATCH",
 //   headers: {
 //     Authorization: "63d45159-fd92-477c-9dd6-c2859fb9ad61",
+//     "Content-Type": "application/json",
 //   },
+//   body: JSON.stringify({
+//     avatar:
+//       "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_avatar.jpg",
+//   }),
 // })
 //   .then((res) => res.json())
 //   .then((result) => {
 //     console.log(result);
-//   })
+//   });
+
+
+export default class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "GET",
+      headers: this._headers,
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+    getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+    setUserInfo(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+    addCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+    deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+    likeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "POST",
+      headers: this._headers,
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+    unlikeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => {
+        if (res.ok) return res.json();
+    }).catch((err) => console.log(err));
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://around-api.pt-br.tripleten-services.com/v1",
+  headers: {
+    Authorization: "63d45159-fd92-477c-9dd6-c2859fb9ad61",
+    "Content-Type": "application/json",
+  },
+});
+// api.getInitialCards().then((result) => console.log(result));
+// api.deleteCard("69c2c7a3084d88001b0aedbc").then((result) => console.log(result))
